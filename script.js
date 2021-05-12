c58e76f24aec77cd302e8e347025d413837e53d5
lokeshPathak3434 <lokeshp3434@gmail.com>
2021-05-12 21:33:49 +0930

    responsive design added

---
+++ script.js
@@ -0,0 +1,13 @@
+let nav=document.querySelector("#navLinks");
+let menuBtn=document.querySelector("#menuBtn");
+let closeBtn=document.querySelector("#closeBtn");
+menuBtn.addEventListener('click',()=>
+{
+    console.log("menu clicked");
+   nav.classList.add("show");
+})
+closeBtn.addEventListener('click',()=>
+{
+    console.log("close clicked")
+    nav.classList.remove("show");
+})
